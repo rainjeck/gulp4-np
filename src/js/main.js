5c42a73004ce24bb60a4d1d245c9a3c590a5baac
Tishuk Nadezda <rainjeck@users.noreply.github.com>
2019-07-20 12:24:13 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@
       svg4everybody();
 
       console.log("work");
+    },
+    _getWinSize: () => {
+      const w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      const h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      return {w, h};
     }
   };
 
